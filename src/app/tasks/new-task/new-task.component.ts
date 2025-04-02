48553fce183145e9f5f66cd794abf37966f99cc8
Raid Thabet <thabet.raid123@gmail.com>
2025-04-02 15:37:08 +0100

    Services

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Output, signal} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {NewTaskData} from '../task/task.model';
+import {TasksService} from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,25 +12,28 @@ import {NewTaskData} from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>(); // void means no data will be emitted
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>(); // void means no data will be emitted
   enteredTitle: string = "";
   enteredSummary: string = "";
   enteredDate: string = "";
+
+  private tasksService = inject(TasksService); // alternative to the constructor-based approach
 
   // enteredTitle= signal("");   |
   // enteredSummary= signal("");  } no need to change anything in the template if we use signals
   // enteredDate= signal("");    |
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       dueDate: this.enteredDate
-    });
+    }, this.userId);
+    this.close.emit();
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {TaskComponent} from './task/task.component';
 import {NewTaskComponent} from './new-task/new-task.component';
 import {NewTaskData} from './task/task.model';
+import {TasksService} from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,58 +17,22 @@ export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
   isAddingTask: boolean = false;
+  private tasksService: TasksService;
 
-  tasks = [
-    {
-      id: "t1",
-      userId: "u1",
-      title: "Master Angular",
-      summary: "Learn all the basics and advanced features of Angular & how to apply them",
-      dueDate: "2025-05-31"
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
-
-  get selectedUserTasks() {
-    return this.tasks.filter(task => task.userId === this.userId);
+  // dependency injection
+  constructor(tasksService: TasksService) {
+    this.tasksService = tasksService;
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
-
-  onAddTask(data: NewTaskData) {
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      dueDate: data.dueDate,
-      summary: data.summary,
-      title: data.title
-    });
-    this.isAddingTask = false;
-  }
-
-  protected readonly onsubmit = onsubmit;
 }
