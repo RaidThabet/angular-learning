5a291edf41b12ab9a3b461dee35c9e00db206125
Raid Thabet <thabet.raid123@gmail.com>
2025-04-05 14:57:27 +0100

    Third version of the solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import {Component, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {UserInputComponent} from './user-input/user-input.component';
-import {InvestmentInput} from './investment-input.model';
 import {InvestmentResultsComponent} from './investment-results/investment-results.component';
 
 @Component({
@@ -15,36 +14,4 @@ import {InvestmentResultsComponent} from './investment-results/investment-result
   ]
 })
 export class AppComponent {
-  resultsData = signal<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[] | undefined>(undefined);
-
-  calculateInvestmentResults(data: InvestmentInput) {
-    const annualData = [];
-    let investmentValue = data.initialInvestment;
-
-    for (let i = 0; i < data.duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
-      investmentValue += interestEarnedInYear + data.annualInvestment;
-      const totalInterest =
-        investmentValue - data.annualInvestment * year - data.initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: data.annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
-      });
-    }
-
-    // console.log(annualData);
-    this.resultsData.set(annualData);
-  }
 }
