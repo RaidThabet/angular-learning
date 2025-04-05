f7fca22ed4696c31920560bdfeb69b50955cac1c
Raid Thabet <thabet.raid123@gmail.com>
2025-04-05 10:46:46 +0100

    Start of Section 4: Angular Essentials Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,8 @@
-import {Component} from '@angular/core';
-import {HeaderComponent} from './header/header.component';
-import {UserComponent} from './user/user.component';
-import {DUMMY_USERS} from "../utils/dummy-users";
-import {TasksComponent} from './tasks/tasks.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: false,
-  // imports: [
-  //   HeaderComponent,
-  //   UserComponent,
-  //   TasksComponent
-  // ],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-
-  selectedUserId?: string;
-
-  get userName() {
-    return this.users.find(user => user.id === this.selectedUserId)?.name ?? "";
-  }
-
-  onSelectUser(id: string) {
-    this.selectedUserId = id;
-  }
-}
+export class AppComponent {}
