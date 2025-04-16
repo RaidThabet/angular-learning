9f43d2924523cb0d3b3853a4d0e76163427b5fea
Raid Thabet <thabet.raid123@gmail.com>
2025-04-16 18:51:18 +0100

    Extending built-in components

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NewTicketComponent} from './new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [
+    NewTicketComponent
+  ],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]', // any button element that has the appButton attribute on it should be controller by this component
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
