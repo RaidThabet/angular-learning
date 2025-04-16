e44e97b1eaeab115a024acbbdce3bc6ab5ac6ab1
Raid Thabet <thabet.raid123@gmail.com>
2025-04-16 19:08:23 +0100

    Content projection with multiple slots

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'button[appButton]', // any button element that has the appButton attribute on it should be controller by this component
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
 }
