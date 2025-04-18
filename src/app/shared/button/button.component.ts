bbfa680c45d3bab749316ad151b9a3c37f8ae4db
Raid Thabet <thabet.raid123@gmail.com>
2025-04-18 12:55:55 +0100

    Scoping CSS styles, and view encapsulation

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]', // any button element that has the appButton attribute on it should be controller by this component
+  // any button or anchor tag element that has the appButton attribute on it should be controller by this component
+  selector: 'button[appButton], a[appButton]',
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
