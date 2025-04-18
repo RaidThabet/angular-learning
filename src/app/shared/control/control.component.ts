183a7d3522d3622759c771674783455685bae80a
Raid Thabet <thabet.raid123@gmail.com>
2025-04-18 15:14:38 +0100

    Alternative way to interact with host elements

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, Input, ViewEncapsulation} from '@angular/core';
+import {Component, HostBinding, HostListener, input, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,19 @@ import {Component, input, Input, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   // these will be added as attributes to the element
   host: {
-    class: "control"
+    class: "control",
+    "(click)": "onClick()"
   }
 })
 export class ControlComponent {
+  // !! Not recommended
+  // @HostBinding("class") className = "control"; // add className as an attribute to the host element and bind to the class property
+  // @HostListener("click") onClick() {
+  //   console.log("clicked!!")
+  // }
   label = input.required<string>();
-  protected readonly input = input;
+
+  onClick() {
+    console.log("clicked!!")
+  }
 }
