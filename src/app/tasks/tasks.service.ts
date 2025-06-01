121f73fc4ef2fc0c0e2e756092cf2e43331761e2
Raid Thabet <thabet.raid123@gmail.com>
2025-06-01 23:21:32 +0100

    Outsourcing and reusing logic with services

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,26 @@
+import {Injectable, signal} from '@angular/core';
+import {Task, TaskStatus} from './task.model';
+
+@Injectable({
+  providedIn: "root"
+})
+export class TasksService {
+  private tasks = signal<Task[]>([]);
+
+  allTasks = this.tasks.asReadonly();
+
+  addTAsk(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: "OPEN"
+    }
+    this.tasks.update((prev) => [...prev, newTask]);
+  }
+
+  updateTasksStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update(
+      (prev) => prev.map((task) => task.id === taskId ? {...task, status: newStatus} : task)
+    );
+  }
+}
