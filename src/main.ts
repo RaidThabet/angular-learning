e5a9db558f9d04d8a8838c36304ca5368e1f2cba
Raid Thabet <thabet.raid123@gmail.com>
2025-06-02 11:48:36 +0100

    Using custom dependency injection token and providers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import {TasksService} from './app/tasks/tasks.service';
+import {InjectionToken} from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+export const TasksServiceToken = new InjectionToken<TasksService>("tasks-service-token");
+
+bootstrapApplication(AppComponent, {
+  // providers: [TasksService] // short way (TasksService is used as the injection token here)
+  providers: [{provide: TasksServiceToken, useClass: TasksService}]
+}).catch((err) => console.error(err));
