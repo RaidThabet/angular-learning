0ca05e3d4f7c2ba120fc83c71b013d1e79b14cec
Raid Thabet <thabet.raid123@gmail.com>
2025-06-03 20:08:57 +0100

    Start of Section 10: Making Sense of Change Detection

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import {TasksService} from './app/tasks/tasks.service';
-import {InjectionToken} from '@angular/core';
 
-export const TasksServiceToken = new InjectionToken<TasksService>("tasks-service-token");
-
-bootstrapApplication(AppComponent, {
-  // providers: [TasksService] // short way (TasksService is used as the injection token here)
-  providers: [{provide: TasksServiceToken, useClass: TasksService}]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
