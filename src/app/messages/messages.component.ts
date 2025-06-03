d46e3c16446014fa108304967792f8dda35f4f67
Raid Thabet <thabet.raid123@gmail.com>
2025-06-03 20:57:24 +0100

    OnPush strategy

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { NewMessageComponent } from './new-message/new-message.component';
@@ -9,6 +9,10 @@ import { NewMessageComponent } from './new-message/new-message.component';
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
   imports: [MessagesListComponent, NewMessageComponent],
+  // Only ever change because some event occurred inside this subcomponent tree, or
+  // because an input value changed in the messages component (in this case, we don't have any input)
+  // NOTE: OnPush does NOT restrict events from affecting other components
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesComponent {
   messages = signal<string[]>([]);
