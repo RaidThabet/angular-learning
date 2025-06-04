f465ebe5cdd87f942cbf035f89ad878154635d1f
Raid Thabet <thabet.raid123@gmail.com>
2025-06-04 15:37:13 +0100

    Implementing app-wide error management

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,11 +3,13 @@ import {inject, Injectable, signal} from '@angular/core';
 import {Place} from './place.model';
 import {catchError, map, tap, throwError} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {ErrorService} from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private errorService = inject(ErrorService);
   private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
@@ -42,6 +44,7 @@ export class PlacesService {
       .pipe(
         catchError(err => {
           this.userPlaces.set(previousPlaces);
+          this.errorService.showError("Failed to store selected place.");
           return throwError(() => new Error("Failed to store selected place."));
         })
       )
