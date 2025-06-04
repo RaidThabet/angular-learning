4b90fa15540bd6c05988e48c378098a5cc608a34
Raid Thabet <thabet.raid123@gmail.com>
2025-06-04 12:11:42 +0100

    Providing the HttpClient and sending a GET request to fetch data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import {Component, DestroyRef, inject, signal} from '@angular/core';
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {Place} from '../place.model';
+import {PlacesComponent} from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
@@ -13,4 +14,18 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.httpClient.get<{ places: Place[] }>("http://localhost:3000/places").subscribe({
+      next: (resData) => {
+        console.log(resData);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
