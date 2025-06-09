ef41a35a98319d70dd563deba44ad5d5c9c1ff6d
Raid Thabet <thabet.raid123@gmail.com>
2025-06-09 19:00:52 +0100

    Resolving route-related dynamic data

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,12 @@
-import {Component, computed, DestroyRef, inject, input, OnInit} from '@angular/core';
+import {Component, inject, input, OnInit} from '@angular/core';
 import {UsersService} from '../users.service';
-import {ActivatedRoute, RouterLink, RouterOutlet} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot
+} from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -12,33 +18,15 @@ import {ActivatedRoute, RouterLink, RouterOutlet} from '@angular/router';
     RouterLink
   ]
 })
-export class UserTasksComponent implements OnInit {
-  // The component instance will be re-used.
-  // Therefore, ngONInit will NOT be executed again.
-  // Hence, a subscription is needed to be notified about changes.
-
-  // userId = input.required<string>(); // will be set by Angular
-  userName = "";
+export class UserTasksComponent {
+  userName = input.required<string>();
   message = input.required<string>();
-  private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
 
-  // userName = computed(
-  //   () => this.usersService.users.find(
-  //     u => u.id === this.userId()
-  //   )?.name
-  // );
+}
 
-  ngOnInit() {
-    console.log("Input data: ", this.message());
-    console.log(this.activatedRoute);
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: paramMap => {
-        this.userName = this.usersService.users.find(u => u.id === paramMap.get("userId"))?.name || ""
-      }
-    });
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+// will be re-executed every time the route parameter changes, so we don't have to setup a subscription
+export const resolveUserName: ResolveFn<string> = (activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot) => {
+  const usersService = inject(UsersService);
+  const userName = usersService.users.find(u => u.id === activatedRoute.paramMap.get("userId"))?.name || "";
+  return userName;
 }
