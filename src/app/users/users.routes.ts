a2940bc12ab217d4c16fe752d52aa7980541c39e
Raid Thabet <thabet.raid123@gmail.com>
2025-06-09 22:06:18 +0100

    Reloading pages via the Angular router and configuring programmatic navigation

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -12,7 +12,7 @@ export const usersRoutes: Routes = [
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
-    runGuardsAndResolvers: "paramsOrQueryParamsChange",
+    runGuardsAndResolvers: "always",
     resolve: {
       userTasks: resolveUserTasks,
     },
