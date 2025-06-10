a259806f5643c9321bb92bb41c33ec78f80b801c
Raid Thabet <thabet.raid123@gmail.com>
2025-06-10 18:14:39 +0100

    Lazy loading entire route groups

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,31 +1,7 @@
-import {ResolveFn, Routes} from '@angular/router';
-import {inject} from '@angular/core';
+import {Routes} from '@angular/router';
 
 import {canLeaveEditPage, NewTaskComponent} from '../tasks/new-task/new-task.component';
-import {Task} from '../tasks/task/task.model';
-import {TasksService} from '../tasks/tasks.service';
-
-const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
-
+import {resolveUserTasks, TasksComponent} from '../tasks/tasks.component';
 
 export const usersRoutes: Routes = [
   {
@@ -35,7 +11,7 @@ export const usersRoutes: Routes = [
   },
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: () => import("../tasks/tasks.component").then(mod => mod.TasksComponent),
+    component: TasksComponent,
     runGuardsAndResolvers: "always",
     resolve: {
       userTasks: resolveUserTasks,
